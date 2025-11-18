425ac1086cca4f2de964c3988a7c7fa30d41df37
doyounggil <tlfkr45@naver.com>
2025-11-18 09:24:46 +0900

    Args

---
+++ Java18_Args/src/MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("args의 개수 : %d\n", args.length);
+		
+		for(String str1 : args) {
+			System.out.println(str1);
+		}
+	}
+
+}
